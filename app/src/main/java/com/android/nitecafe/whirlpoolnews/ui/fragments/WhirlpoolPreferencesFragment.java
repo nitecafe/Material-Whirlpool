c8e19ec4fc2533247eb6a5f7cc9218651dc1c81d
nitecafe <nitecafe@gmail.com>
2016-02-24 22:54:33 +1100

    Update preference layout to material style

--- app/src/main/java/com/android/nitecafe/whirlpoolnews/ui/fragments/WhirlpoolPreferencesFragment.java
+++ app/src/main/java/com/android/nitecafe/whirlpoolnews/ui/fragments/WhirlpoolPreferencesFragment.java
@@ -1,6 +1,8 @@
 package com.android.nitecafe.whirlpoolnews.ui.fragments;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 import com.android.nitecafe.whirlpoolnews.R;
@@ -10,5 +12,13 @@ public class WhirlpoolPreferencesFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
+        setToolbarTitle();
+    }
+
+    private void setToolbarTitle() {
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        ActionBar supportActionBar = activity.getSupportActionBar();
+        if (supportActionBar != null)
+            supportActionBar.setTitle("Settings");
     }
 }
